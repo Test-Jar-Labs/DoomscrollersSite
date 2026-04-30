b4139f6c1a5c0bbc64d2895f03bbb0fdd20aee64
emmmjiang <jemilyiauc@gmail.com>
2026-04-29 17:53:42 -0700

    fix text links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../App.module.css'
+
+function ComingSoonLink({ children }) {
+  const [hovered, setHovered] = useState(false)
+  const [clicked, setClicked] = useState(false)
+  return (
+    <a
+      href="#"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={(e) => { e.preventDefault(); setClicked(c => !c) }}
+    >
+      {hovered || clicked ? 'not available... yet' : children}
+    </a>
+  )
+}
 
 export default function Home() {
   return (
@@ -9,8 +25,8 @@ export default function Home() {
         a privacy invading battle of<br />digital profiling
       </p>
       <nav className={styles.links}>
-        <a href="#">download on iOS</a>
-        <a href="#">download on Google Play</a>
+        <ComingSoonLink>download on iOS</ComingSoonLink>
+        <ComingSoonLink>download on Google Play</ComingSoonLink>
         <Link to="/privacy">privacy policy &amp; terms</Link>
         <Link to="/faq">faq</Link>
       </nav>
